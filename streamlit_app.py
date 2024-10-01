7d28720910f921115eb6c76e14f93e7dde9752c1
saiDinesh22048 <118376163+saiDinesh22048@users.noreply.github.com>
2024-10-02 00:58:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,74 +44,73 @@ with st.sidebar:
   gls_lvl = st.slider('Glucose level', 0, 600, 85)
   bmi = st.slider('Bmi', 5, 60, 30)
   smking_stat= st.selectbox('Smoking status', ('never smoked', 'formerly smoked','smokes'))
-  # Create a DataFrame for the input features
-  label_encoder = LabelEncoder()
-  data = {'gender': gender,
-          'age': age,
-          'hypertension': hypo,
-          'heart_disease': heart,
-          'ever_married': marry_status,
-          'work_type': wrk_typ,
-          'avg_glucose_level': gls_lvl,
-          'bmi': bmi,
-          'smoking_status': smking_stat}
-
-
-input_df = pd.DataFrame(data, index=[0])
-input_values = pd.concat([input_df, X_raw], axis=0)
-
-input_values['gender'] = label_encoder.fit_transform(input_values['gender'])
-
-input_values['hypertension'] = label_encoder.fit_transform(input_values['hypertension'])
-
-input_values['heart_disease'] = label_encoder.fit_transform(input_values['heart_disease'])
-
-input_values['ever_married'] = label_encoder.fit_transform(input_values['ever_married'])
-
-input_values['smoking_status'] = input_values['smoking_status'].fillna('Unknown')
-
-input_values['smoking_status'] = label_encoder.fit_transform(input_values['smoking_status'])
-
-input_values['work_type'] = label_encoder.fit_transform(input_values['work_type'])
-
-
-scaler =StandardScaler()
-input_values = scaler.fit_transform(input_values)
-
-arr = np.array(input_values)
-input =arr[0, :]
-input_values =arr[1:, :]
-
-with st.expander('Input features'):
-  st.write('**Input values**')
-  input_df
-  st.write('**Combined input data**')
-  input_values
-
-smote_enn = SMOTEENN()
-X_res1, y_res1 = smote_enn.fit_resample(input_values ,y_raw)
-input = input.reshape(1, -1)
-input
-input_values
-
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import roc_curve,auc
-
-# Splitting the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X_res1, y_res1, test_size=0.2, random_state=42)
-
-# Initializing the Random Forest classifier
-rf_classifier = RandomForestClassifier(random_state=42)
-
-# Training the classifier
-rf_classifier.fit(X_train, y_train)
-
-# Making predictions
-y = rf_classifier.predict(input)
-if y[0]==0:
-  st.success("congrats,you have less risk for cerebral stroke")
-else:
-  st.warning("you have high risk for cerebral stroke.please consult a neurologist immideatly")
+if st.button("predict"):
+    # Create a DataFrame for the input features
+    label_encoder = LabelEncoder()
+    data = {'gender': gender,
+            'age': age,
+            'hypertension': hypo,
+            'heart_disease': heart,
+            'ever_married': marry_status,
+            'work_type': wrk_typ,
+            'avg_glucose_level': gls_lvl,
+            'bmi': bmi,
+            'smoking_status': smking_stat}
+    
+    
+    input_df = pd.DataFrame(data, index=[0])
+    input_values = pd.concat([input_df, X_raw], axis=0)
+    
+    input_values['gender'] = label_encoder.fit_transform(input_values['gender'])
+    
+    input_values['hypertension'] = label_encoder.fit_transform(input_values['hypertension'])
+    
+    input_values['heart_disease'] = label_encoder.fit_transform(input_values['heart_disease'])
+    
+    input_values['ever_married'] = label_encoder.fit_transform(input_values['ever_married'])
+    
+    input_values['smoking_status'] = input_values['smoking_status'].fillna('Unknown')
+    
+    input_values['smoking_status'] = label_encoder.fit_transform(input_values['smoking_status'])
+    
+    input_values['work_type'] = label_encoder.fit_transform(input_values['work_type'])
+    
+    
+    scaler =StandardScaler()
+    input_values = scaler.fit_transform(input_values)
+    
+    arr = np.array(input_values)
+    input =arr[0, :]
+    input_values =arr[1:, :]
+    
+    with st.expander('Input features'):
+      st.write('**Input values**')
+      input_df
+      st.write('**Combined input data**')
+      input_values
+    
+    smote_enn = SMOTEENN()
+    X_res1, y_res1 = smote_enn.fit_resample(input_values ,y_raw)
+    input = input.reshape(1, -1)
+    
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import accuracy_score, classification_report
+    from sklearn.metrics import confusion_matrix
+    from sklearn.metrics import roc_curve,auc
+    
+    # Splitting the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X_res1, y_res1, test_size=0.2, random_state=42)
+    
+    # Initializing the Random Forest classifier
+    rf_classifier = RandomForestClassifier(random_state=42)
+    
+    # Training the classifier
+    rf_classifier.fit(X_train, y_train)
+    
+    # Making predictions
+    y = rf_classifier.predict(input)
+    if y[0]==0:
+      st.success("congrats,you have less risk for cerebral stroke")
+    else:
+      st.warning("you have high risk for cerebral stroke.please consult a neurologist immideatly")
